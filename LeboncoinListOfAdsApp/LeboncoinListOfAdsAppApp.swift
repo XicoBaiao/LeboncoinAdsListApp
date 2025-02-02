bd73934e2f2bf7b9775369bd370c73fcc31c3596
Francisco Baião <fbaiao@deloitte.pt>
2025-02-02 14:42:14 +0000

    First commit

---
+++ LeboncoinListOfAdsApp/LeboncoinListOfAdsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeboncoinListOfAdsAppApp.swift
+//  LeboncoinListOfAdsApp
+//
+//  Created by Francisco Baião on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeboncoinListOfAdsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeboncoinListOfAdsAppUITests/LeboncoinListOfAdsAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LeboncoinListOfAdsAppUITestsLaunchTests.swift
+//  LeboncoinListOfAdsAppUITests
+//
+//  Created by Francisco Baião on 02/02/2025.
+//
+
+import XCTest
+
+final class LeboncoinListOfAdsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
