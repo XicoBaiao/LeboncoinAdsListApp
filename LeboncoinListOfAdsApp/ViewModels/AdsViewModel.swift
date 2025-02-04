989a3282e08cc89db0885013f64df538cb289efe
Francisco Baião <fbaiao@deloitte.pt>
2025-02-04 13:27:39 +0000

    Add AdsViewModel to handle API logic

---
+++ LeboncoinListOfAdsApp/ViewModels/AdsViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  AdsViewModel.swift
+//  LeboncoinListOfAdsApp
+//
+//  Created by Francisco Baião on 04/02/2025.
+//
+
+import Foundation
+import SwiftUI
+
+class AdsViewModel: ObservableObject {
+    @Published var ads: [Ad] = []
+    @Published var categories: [Category] = []
+    @Published var errorMessage: String?
+
+    private let apiService: APIServiceProtocol
+
+    init(apiService: APIServiceProtocol = APIService.shared) {
+        self.apiService = apiService
+    }
+
+    func loadAds() {
+        apiService.fetchAds { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let ads):
+                if !ads.isEmpty {
+                    self.ads = ads
+                }
+            case .failure(let error):
+                switch error {
+                case NetworkError.notFound:
+                    self.errorMessage = "Endpoint not found"
+                case NetworkError.badRequest:
+                    self.errorMessage = "Bad Request"
+                case NetworkError.serverError:
+                    self.errorMessage = "Server error"
+                case NetworkError.notAuthorized:
+                    self.errorMessage = "Not authorized"
+                default:
+                    self.errorMessage = "An error occurred"
+                }
+            }
+        }
+    }
+
+    func loadCategories() {
+        apiService.fetchCategories { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let categories):
+                if !categories.isEmpty {
+                    self.categories = categories
+                }
+            case .failure(let error):
+                switch error {
+                case NetworkError.notFound:
+                    self.errorMessage = "Endpoint not found"
+                case NetworkError.badRequest:
+                    self.errorMessage = "Bad Request"
+                case NetworkError.serverError:
+                    self.errorMessage = "Server error"
+                case NetworkError.notAuthorized:
+                    self.errorMessage = "Not authorized"
+                default:
+                    self.errorMessage = "An error occurred"
+                }
+            }
+        }
+    }
+}
+
